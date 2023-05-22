a64e88a6f5c3b76e661c8dae2e42d4d3e1fdfc03
li5bo5 <li5bo5@163.com>
2023-05-22 09:59:56 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,33 @@
 <?php
-$dir =  dirname(__FILE__);
-$file = scandir($dir);
-$s=sizeof($file);
-$x=rand(0,$s);
-while(substr($file[$x],-3)!="jpg" && substr($file[$x],-3)!="png"){
-	$x=rand(0,$s);
+//存有美图链接的文件名img.txt
+$filename = "img.txt";
+if(!file_exists($filename)){
+    die('文件不存在');
 }
-$img="http://li5bo5.github.io/".$file[$x];
-// echo "<img src=$img>";
-header("location:$img");
+ 
+//从文本获取链接
+$pics = [];
+$fs = fopen($filename, "r");
+while(!feof($fs)){
+    $line=trim(fgets($fs));
+    if($line!=''){
+        array_push($pics, $line);
+    }
+}
+ 
+//从数组随机获取链接
+$pic = $pics[array_rand($pics)];
+ 
+//返回指定格式
+$type=$_GET['type'];
+switch($type){
+ 
+//JSON返回
+case 'json':
+    header('Content-type:text/json');
+    die(json_encode(['pic'=>$pic]));
+ 
+default:
+    die(header("Location: $pic"));
+}
 ?>
